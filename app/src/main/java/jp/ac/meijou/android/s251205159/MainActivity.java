376eb8586baa7c39626889a0977a3d9edaa5dfb5
フレンキー <251205159@ccmailg.meijo-u.ac.jp>
2026-09-01 17:56:39 +0900

    授業内容を変更しました

--- app/src/main/java/jp/ac/meijou/android/s251205159/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s251205159/MainActivity.java
@@ -17,75 +17,58 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private PrefDataStore dataStore;
 
-    /**
-     * @param savedInstanceState 以前に保存された状態データ
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.item_2), (v, insets) -> {
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-    TextView text = findViewById(R.id.item_12);
-        text.setText("よろしくお願いします");
-        binding.item12.setText("こんにちは");
-        binding.change1.setOnClickListener(view -> {
-            binding.save1.setText("はーい！");
+        dataStore = PrefDataStore.getInstance(getApplicationContext());
+
+        binding.item12.setText("よろしくお願いします");
+        dataStore.getString("text").ifPresent(text -> {
+            if ("a".equals(text)) {
+                binding.textview1.setText("Aの画像です");
+                binding.imageView3.setImageResource(R.drawable.baseline_drive_eta_24);
+            } else if ("b".equals(text)) {
+                binding.textview1.setText("Bの画像です");
+                binding.imageView3.setImageResource(R.drawable.ic_launcher_background);
+            } else {
+                binding.textview1.setText("知らない画像です");
+                binding.imageView3.setImageResource(R.drawable.ic_launcher_foreground);
+            }
         });
+        // 保存済みの名前があれば読み込んで表示
+        dataStore.getString("name")
+                .ifPresent(name -> binding.item12.setText(name));
+
         setOnClickListener();
-        }
-        private void setOnClickListener() {
-            binding.change1.setOnClickListener(view -> {
-                var text = binding.name1.getText();
-                binding.item12.setText(text);
-            });
-        }
-        private void setTextChangedListener() {
-            binding.name1.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+    }
 
-            }
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+    private void setOnClickListener() {
+        // Changeボタン: EditTextの内容をitem12に反映するだけ（保存はしない）
+        binding.item2.setOnClickListener(view -> {
+            var text = binding.name1.getText().toString();
+            binding.item12.setText(text);
+        });
 
-            }
-
-             @Override
-                public void afterTextChanged(Editable editable) {
-                binding.name1.setText(editable.toString());
-                }
-            });
-            dataStore = PrefDataStore.getInstance(getApplicationContext());
-
-            // "name" というキーで保存されている文字列を読み込み、存在すればTextViewにセットする
-            dataStore.getString("name")
-                    .ifPresent(name -> binding.save1.setText(name));
-
-            // Changeボタンが押されたら、EditTextの内容をTextViewに反映する（保存はしない）
-            binding.save1.setOnClickListener(view -> {
-                var text = binding.name1.getText().toString();
-                binding.item12.setText(text);
-            });
-
-            binding.save1.setOnClickListener(view -> {
-                var text = binding.change1.getText().toString();
-                dataStore.setString("name", text);
-            });
-        }
+        // Saveボタン: EditTextの内容をitem12に反映し、データストアに保存する
+        binding.save1.setOnClickListener(view -> {
+            var text = binding.name1.getText().toString();
+            binding.item12.setText(text);
+            dataStore.setString("name", text);
+        });
+    }
 
     @Override
     protected void onStart() {
         super.onStart();
-//        dataStore.getString("name")
-//                .ifPresent(name -> binding.lesson5Text.setText(name));
     }
 }
-
-
